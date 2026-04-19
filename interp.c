173b8ebdeb8cc8fc3c49794df9feb0665ecae4ff
paulboul1013 <paulboul1013@gmail.com>
2026-04-19 11:25:17 +0800

    Interpre AST tree structure

---
+++ interp.c
@@ -0,0 +1,42 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+//list of AST operators
+char *ASTop[]={"+","-","*","/"};
+
+//given AST, interpret the operators in the tree and return a final value
+int interpretAST(struct ASTnode *n){
+    int leftval,rightval;
+
+    //get the left and right sub-tree values
+    if (n->left){
+        leftval=interpretAST(n->left);
+    }
+    if (n->right){
+        rightval=interpretAST(n->right);
+    }
+
+    //Debug: print operator and values
+    if (n->op==A_INTLIT){
+        printf("int %d\n",n->intvalue);
+    }else{
+        printf("%d %s %d\n",leftval,ASTop[n->op],rightval);
+    }
+
+    switch (n->op){
+        case A_ADD:
+            return leftval+rightval;
+        case A_SUBTRACT:
+            return leftval-rightval;
+        case A_MULTIPLY:
+            return leftval*rightval;
+        case A_DIVIDE:
+            return leftval/rightval;
+        case A_INTLIT:
+            return n->intvalue;
+        default:
+            fprintf(stderr,"unkown AST operator %d\n",n->op);
+            exit(1);
+    }
+}
