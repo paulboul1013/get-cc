99c7b3a923d4b853c56cb43fe6ed827fe38cad12
paulboul1013 <paulboul1013@gmail.com>
2026-04-19 11:24:30 +0800

    Add AST node convert expression function

---
+++ expr.c
@@ -0,0 +1,64 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+//convert a token into an AST operation
+static int arithop(int tok){
+    switch (tok){
+        case T_PLUS:
+            return A_ADD;
+        case T_MINUS:
+            return A_SUBTRACT;
+        case T_STAR:
+            return A_MULTIPLY;
+        case T_SLASH:
+            return A_DIVIDE;
+        default:
+            fprintf(stderr, "unknown token in arithop() on line %d\n", Line);
+            exit(1);
+    }
+}
+
+//parse a primary factor and return an AST node representing it
+static struct ASTnode *primary(void){
+    struct ASTnode *n;
+
+    //for an INTLIT token, make a leaf AST node for it
+    switch (Token.token){
+        case T_INTLIT:
+            n = mkastleaf(A_INTLIT, Token.intvalue);
+            scan(&Token);
+            return n;
+        default:
+            fprintf(stderr, "syntax error on line %d\n", Line);
+            exit(1);
+    }
+}
+
+//return an AST tree whose root is a binary operator
+struct ASTnode *binexpr(void){
+    struct ASTnode *n, *left, *right;
+    int nodetype;
+
+    //get the integer literal on the left
+    //fetch the next token at the same time
+    left = primary();
+
+    //if no tokens left, return just the left node
+    if (Token.token == T_EOF){
+        return left;
+    }
+
+    //convert the token into a node type
+    nodetype = arithop(Token.token);
+
+    //get the next token
+    scan(&Token);
+
+    //recursively get the right-hand tree
+    right = binexpr();
+
+    //now build a tree with both sub-trees
+    n = mkastnode(nodetype, left, right, 0);
+    return n;
+}
